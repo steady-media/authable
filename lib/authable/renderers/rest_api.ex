c546446f09888f04812599dd20d7359a35245dda
Mustafa TURAN <mustafaturan.net@gmail.com>
2016-08-01 23:49:23 +0100

    Update error renderer.

--- lib/authable/renderers/rest_api.ex
+++ lib/authable/renderers/rest_api.ex
@@ -11,6 +11,12 @@ defmodule Authable.Renderer.RestApi do
   def render(conn, status, map) do
     conn
     |> put_resp_content_type("application/json", "utf-8")
-    |> resp(status, Poison.encode!(map))
+    |> resp(status, Poison.encode!(merge_error_keys(map)))
+  end
+
+  defp merge_error_keys(errors) do
+    Enum.reduce(errors, %{}, fn({key, val}, acc) ->
+      Map.update(acc, key, [val], &[val|&1])
+    end)
   end
 end
