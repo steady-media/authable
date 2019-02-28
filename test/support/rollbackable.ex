ec893c19437c3cf33c412417154b17d9cf937881
Manuel Kallenbach <manuel@steadyhq.com>
2019-02-28 12:07:50 +0100

    delete commented out code

--- test/support/rollbackable.ex
+++ test/support/rollbackable.ex
@@ -15,7 +15,6 @@ defmodule Authable.Rollbackable do
   setup do
     # Wrap this case in a transaction
     Ecto.Adapters.SQL.Sandbox.mode(@repo, :manual)
-    # :ok = Ecto.Adapters.SQL.Sandbox.checkout(@repo)
     Ecto.Adapters.SQL.Sandbox.checkout(@repo)
 
     # Roll it back once we are done
